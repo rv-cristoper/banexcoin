cb5bab9aa3f116447643bf969baaf6a1e6570251
cristoper <jhanfranco01@gmail.com>
2021-12-21 22:09:48 -0500

    feat: first commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import HelloWorld from "./components/HelloWorld";
+// import RenderForm from "./components/RenderForm";
+
+const App: React.FC<{}> = () => {
+  return (
+    <>
+      {/* <HelloWorld />
+      <RenderForm /> */}
+      <h1>BanexCoin</h1>
+    </>
+  );
+};
+
+export default App;
